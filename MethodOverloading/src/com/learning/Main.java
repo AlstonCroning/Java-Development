8cc2aacaf956b228fedb2df6327a5a43d5f2013c
Alston <and_croning@hotmail.com>
2016-02-06 01:44:25 -0500

    Method Overloading feature to be continued...

--- MethodOverloading/src/com/learning/Main.java
+++ MethodOverloading/src/com/learning/Main.java
@@ -3,6 +3,27 @@ package com.learning;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New Score is " + newScore);
+        calculateScore(75);
+        calculateScore();
     }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("player " + playerName + " scored " + score
+        + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("unknown player scored " + score
+                + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("no player name, no player score.");
+        return 0;
+    }
+
 }
