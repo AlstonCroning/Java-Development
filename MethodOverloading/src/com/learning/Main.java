9dfc02aee99b6f8f7843fadd104687c3e1510ebe
Alston <and_croning@hotmail.com>
2016-02-09 02:44:48 -0500

    Method Overloading feature to be continued...

--- MethodOverloading/src/com/learning/Main.java
+++ MethodOverloading/src/com/learning/Main.java
@@ -7,7 +7,53 @@ public class Main {
         System.out.println("New Score is " + newScore);
         calculateScore(75);
         calculateScore();
+
+        double centimeters = calcFeetAndInchesToCentimeters(6,-1);
+
+        if(centimeters < 0.0)
+        {
+            System.out.println("Invalid Parameters");
+        }
     }
+
+
+    public static double calcFeetAndInchesToCentimeters(double feet,double inches) {
+
+        //calculating cm
+        if(feet < 0 || ( (inches < 0) || (inches > 12) ))
+        {
+            System.out.println("Invalid Feet or Inches Parameters!");
+            return -1;
+        }
+
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + "feet, " + inches + "inches = " + centimeters + "cm");
+        return centimeters;
+    }
+
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+
+        double cm;
+        double feet;
+
+        //calculating feet
+        if(inches >= 0)
+        {
+            cm = inches * 2.54; //converted to cm
+            feet = cm * 0.032808;//converted to feet
+            return feet;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+
+
+
 
     public static int calculateScore(String playerName, int score) {
         System.out.println("player " + playerName + " scored " + score
@@ -25,5 +71,6 @@ public class Main {
         System.out.println("no player name, no player score.");
         return 0;
     }
+
 
 }
