759a5aba7b436c3c86f3a714bfc6ff7491cb74b9
Alston <and_croning@hotmail.com>
2016-02-09 17:24:57 -0500

    features/witch to be continued..

--- Switch/src/com/learning/Main.java
+++ Switch/src/com/learning/Main.java
@@ -3,6 +3,23 @@ package com.learning;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int value = 2;
+        if(value == 1) {
+            System.out.println("Value was 1");
+        } else if(value == 2){
+            System.out.println("value was 2");
+        }else {
+            System.out.println("was not 1 or 2");
+        }
+
+        int switchValue = 1;
+
+        switch (switchValue){
+            case 1:
+                System.out.println("Value was 1");
+                break;
+        }
+
+
     }
 }
