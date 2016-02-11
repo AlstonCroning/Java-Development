142ba2026045bf24415ef0a559d4210a8f6c13fd
Alston <and_croning@hotmail.com>
2016-02-11 03:11:34 -0500

    feature/ForLoops to be continued..

--- ForLoops/src/com/learning/Main.java
+++ ForLoops/src/com/learning/Main.java
@@ -3,6 +3,13 @@ package com.learning;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("10,000 at 2% interest = " + caculateInterest(10000.0,2.0));
     }
-}
+
+    //calculate the interest based on the amount of money that's passed from the parameters
+    public static double caculateInterest(double amount,double interestRate) {
+        return (amount * (interestRate / 100));
+    }
+
+}//end of class Main
