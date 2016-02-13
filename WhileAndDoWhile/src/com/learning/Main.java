2bf7af858b77813a81e17727174803df9ba9cc70
Alston <and_croning@hotmail.com>
2016-02-13 02:41:20 -0500

    feature/WhileDoWhile to be continued..

--- WhileAndDoWhile/src/com/learning/Main.java
+++ WhileAndDoWhile/src/com/learning/Main.java
@@ -3,6 +3,30 @@ package com.learning;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //example of a while loop with a condition statement
+        int count = 1;
+        while(count != 6) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+
+        //same example with an alternative way
+        count = 1;
+        while(true) {
+            if(count == 6) {
+                break;
+            }
+            System.out.println("Count value is " + count);
+            count++;
+        }
+
+        //using do while statement with the same example
+        count = 1;
+        do {
+            System.out.println("Count value was " + count);
+            count++;
+        } while (count != 6);
+
     }
 }
