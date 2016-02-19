9defb32d69a9859924c40b73b034b9e274c0395b
Alston <and_croning@hotmail.com>
2016-02-19 01:12:59 -0500

    demonstration of ATM simulator without using constructors in feature/constructors

--- Constructors/src/com/learning/BankAccount.java
+++ Constructors/src/com/learning/BankAccount.java
@@ -1,4 +1,85 @@
 package com.learning;
 
 public class BankAccount {
-}
+
+    private int actNumber;
+    private double balence;
+    private String customerName;
+    private String email;
+    private String phoneNumber;
+
+    //Acount Number (Setter)
+    public void setActNumber (int actNumber) {
+        this.actNumber = actNumber;
+    }
+    //Acount Number (getter)
+    public int getActNumber () {
+        return this.actNumber;
+    }
+
+
+    //Balence (Setter)
+    public void setBalence (double balence) {
+        this.balence = balence;
+    }
+    //Balence (getter)
+    public double getBalence () {
+        return this.balence;
+    }
+
+
+    //CustomerName (setter)
+    public void setCustomerName (String customerName) {
+        this.customerName = customerName;
+    }
+    //CustomerName (getter)
+    public String getCustomerName () {
+        return this.customerName;
+    }
+
+
+    //CustomerName (setter)
+    public void setEmail (String email) {
+        this.email = email;
+    }
+    //CustomerName (getter)
+    public String getEmail () {
+        return this.email;
+    }
+
+
+    //CustomerName (setter)
+    public void setPhoneNumber (String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    //CustomerName (getter)
+    public String getPhoneNumber () {
+        return this.phoneNumber;
+    }
+
+
+    //Deposit Money
+    public void deposit (double deposit) {
+        this.balence += deposit;
+        System.out.println(
+                "Balence = $" + this.balence +
+                ", Money Deposited = $" + deposit
+        );
+    }
+
+    public double withDraw (double withDraw) {
+
+        if(this.balence < withDraw){
+            System.out.println("Insufficient Funds");
+            return -1;
+        } else {
+            this.balence -= withDraw;//deducting balence from the withDraw
+            System.out.println(
+                    "Balence = $" + balence +
+                    ", Amount WithDrawn = $" + withDraw);
+            return 0;
+        }
+    }
+
+
+}//end of BankAccount Class
--- Constructors/src/com/learning/Main.java
+++ Constructors/src/com/learning/Main.java
@@ -3,6 +3,32 @@ package com.learning;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        BankAccount TD = new BankAccount();
+
+        //AcountNumber
+        TD.setActNumber(4562);
+        System.out.println("Acount #: " + TD.getActNumber());
+
+        //Balence
+        TD.setBalence(5000);
+        System.out.println("Balence = $" + TD.getBalence());
+
+        //CustomerName
+        TD.setCustomerName("Dishan");
+        System.out.println("CustomerName: " + TD.getCustomerName());
+
+        //Email
+        TD.setEmail("abc@email.com");
+        System.out.println("Email: " + TD.getEmail());
+
+        //PhoneNumber
+        TD.setPhoneNumber("555555555");
+        System.out.println("Phone #: " + TD.getPhoneNumber());
+
+        //Deposit and Withdraw
+        TD.deposit(200);//money deposited test
+        TD.withDraw(100);//money withDrown test
+        TD.withDraw(5101);//insufficient funds test
+
     }
 }
