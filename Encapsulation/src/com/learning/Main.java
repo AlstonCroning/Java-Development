316945c1b06b89c626d7a4a455feddba25977a92
Alston <and_croning@hotmail.com>
2016-05-21 00:15:01 -0400

    completed Encapsulation exercise

--- Encapsulation/src/com/learning/Main.java
+++ Encapsulation/src/com/learning/Main.java
@@ -3,6 +3,19 @@ package com.learning;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//        Player player = new Player();
+//        player.health = 20;
+//        player.name = "Tim";
+//        player.weapon = "Sword";
+//
+//        player.loseHealth(10);
+//        System.out.println(player.healthRemaining());
+//
+//        player.health = 200;
+//        player.loseHealth(11);
+//        System.out.println(player.healthRemaining());
+
+        EnhancedPlayer enhancedPlayer = new EnhancedPlayer("Dishan",200,"Sward");
+        System.out.println(enhancedPlayer.getHealth());
     }
 }
---
+++ Encapsulation/src/com/learning/Player.java
@@ -0,0 +1,20 @@
+package com.learning;
+
+public class Player {
+
+    public String fullName;
+    public int health;
+    public String weapon;
+
+    public void loseHealth(int damage) {
+        this.health = this.health - damage;
+        if(this.health <= 0) {
+            System.out.println("Player knoked out");
+            //reduce number of lives remaining for the player
+        }
+    }
+
+    public int healthRemaining() {
+        return this.health;
+    }
+}
