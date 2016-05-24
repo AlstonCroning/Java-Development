c8db601c3a40620ef0b1487bbb02d05a1c49a801
Alston <and_croning@hotmail.com>
2016-05-24 18:53:56 -0400

    Duplex and nonDuplex function works

--- EncapsulationChallenge/src/com/AlstonCroning/Printer.java
+++ EncapsulationChallenge/src/com/AlstonCroning/Printer.java
@@ -1,22 +1,24 @@
 package com.AlstonCroning;
 
 public class Printer {
-    private int tonerLevel = 100;
-    private int numOfPagesPrinted = 0;
+    private int tonerLevel;
+    private int numOfPagesPrinted;
     private boolean isDuplex;
 
-    public Printer(int tonerLevel, int numOfPagesPrinted, boolean isDuplex) {
+    public Printer(int tonerLevel, boolean isDuplex) {
         if(tonerLevel >= 0 && tonerLevel <= 100) {
             this.tonerLevel = tonerLevel;
+        } else {
+            this.tonerLevel = -1;
         }
-        this.numOfPagesPrinted = numOfPagesPrinted;
+        this.numOfPagesPrinted = 0;
         this.isDuplex = isDuplex;
     }
 
     public void fillUpToner(int increaseTonerLevel) {
         int tonerDifference = (100 - this.tonerLevel);
          if((increaseTonerLevel <= tonerDifference) && (tonerLevel >= 0 && tonerLevel < 100) ) {
-             this.tonerLevel = tonerLevel + increaseTonerLevel;
+             this.tonerLevel += increaseTonerLevel;
              System.out.println("Tonner Level is increased by " + increaseTonerLevel + "%");
          } else {
              if(tonerLevel == 100) {
@@ -27,15 +29,22 @@ public class Printer {
          }
     }
 
+    public int print(int pages) {
+        int pagesToPrint = pages;
+        if(this.isDuplex) {
+            System.out.println("Printing in duplex printing mode");
+            pagesToPrint /= 2;
+        }
+        System.out.println("Printing Done");
+        this.numOfPagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+
     public int getTonerLevel() {
         return tonerLevel;
     }
 
     public int getNumOfPagesPrinted() {
         return numOfPagesPrinted;
-    }
-
-    public boolean isDuplex() {
-        return isDuplex;
     }
 }
