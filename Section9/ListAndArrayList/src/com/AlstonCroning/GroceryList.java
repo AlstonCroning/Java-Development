7bceef84e59e0ef4f6e17a19d8f9ae6457a71150
Alston <and_croning@hotmail.com>
2016-07-06 18:42:20 -0400

    methods created in groceryList class. to be continued..

---
+++ Section9/ListAndArrayList/src/com/AlstonCroning/GroceryList.java
@@ -0,0 +1,42 @@
+package com.AlstonCroning;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    private int[] mynumbers;
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    public void addGroceryItem(String item) {
+        groceryList.add(item);
+    }
+
+    public void printGrooceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your grocery list");
+        for(int i=0; i<groceryList.size(); i++){
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryList(int position, String newItem) {
+        groceryList.set(position,newItem);
+        System.out.println("Grocery Item " + (position+1) + " has been modified");
+    }
+
+    public void removeGroceryItem(int position) {
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+    }
+
+    public String findItem(String searchItem) {
+        //short cut for determining if an item exist in an arrayList
+//        boolean exists = groceryList.contains(searchItem);
+        int position = groceryList.indexOf(searchItem);
+        if(position >= 0){
+            return groceryList.get(position);
+        }
+        return null;
+    }
+
+
+
+}
