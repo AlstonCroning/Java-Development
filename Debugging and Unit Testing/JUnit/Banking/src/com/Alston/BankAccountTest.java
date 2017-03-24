fdc36548944d11a6b833d427ce116f1bca3e8d61
Dishan <a1xcr0nge145>
2017-03-23 22:09:42 -0400

    Exception Handling demonstration optimized

--- Debugging and Unit Testing/JUnit/Banking/src/com/Alston/BankAccountTest.java
+++ Debugging and Unit Testing/JUnit/Banking/src/com/Alston/BankAccountTest.java
@@ -30,10 +30,12 @@ public class BankAccountTest {
         assertEquals(400.00,balance,0);
     }
 
-    @org.junit.Test(expected = IllegalArgumentException.class)
+    @org.junit.Test (expected = IllegalArgumentException.class)
     public void withdraw_notBranch() throws Exception {
-        double balance = account.withdraw(600.00,false);
-        assertEquals(400.00,balance,0);
+//        double balance = account.withdraw(600.00,false);
+//        assertEquals(400.00,balance,0);
+        account.withdraw(600.00, false);
+        fail("Should have thrown an IllegalArgumentException");
     }
 
 
